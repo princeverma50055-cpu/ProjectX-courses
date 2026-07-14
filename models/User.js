3bc32b237fa34d4b3e801931f1a21589bc4adb59
Prince Verma <princeverma50055@gmail.com>
2026-07-15 00:21:25 +0530

    Create User model with schema definition

---
+++ models/User.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+
+const UserSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  enrolledCourses: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Course' }],
+  createdAt: { type: Date, default: Date.now }
+});
+
+export default mongoose.models.User || mongoose.model('User', UserSchema);
+
